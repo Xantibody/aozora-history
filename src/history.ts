307c443c7e3caa02f4a9c81bc023f394b957375c
Xantibody <zeku.bushinryu38@gmail.com>
2026-07-11 23:39:59 +0900

    chore: remove unused history module left over from the template

--- src/history.ts
+++
@@ -1,9 +0,0 @@
-export interface HistoryEntry {
-  url: string;
-  title: string;
-  visitedAt: number;
-}
-
-export function sortByVisitedAt(entries: HistoryEntry[]): HistoryEntry[] {
-  return entries.toSorted((a, b) => b.visitedAt - a.visitedAt);
-}
